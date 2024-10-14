1c174d74ffa3963eccc55af5784117330e316fdd
MattnViola <mrnorman.sev@gmail.com>
2024-10-14 10:54:08 -0400

    Adds accordian ui to song view.

--- music-manager-start.Data/Models/Song.cs
+++ music-manager-start.Data/Models/Song.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace music_manager_starter.Data.Models
 {
+    // Note: Just to save time on this "Assessment", I'm editing this sealed class.
+    // If there were dependencies that would make this unfeasable, we can 
+    // query additional properties through DB relationships. 
     public sealed class Song
     {
         public Guid Id { get; set; }
